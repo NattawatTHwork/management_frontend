60d18994685f68599adfbe8c2be638a7c5d683f6
NattawatTHwork <nattawatth.work@gmail.com>
2023-12-20 11:14:55 +0700

    time clock

---
+++ src/components/DistanceCalculator.tsx
@@ -0,0 +1,28 @@
+interface Location {
+  latitude: number;
+  longitude: number;
+}
+
+const toRadians = (degrees: number): number => (degrees * Math.PI) / 180;
+
+const DistanceCalculator = (locationA: Location, locationB: Location): number => {
+  const earthRadius = 6371;
+
+  const latA = toRadians(locationA.latitude);
+  const lonA = toRadians(locationA.longitude);
+  const latB = toRadians(locationB.latitude);
+  const lonB = toRadians(locationB.longitude);
+
+  const dLon = lonB - lonA;
+  const dLat = latB - latA;
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(latA) * Math.cos(latB) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return earthRadius * c;
+};
+
+export default DistanceCalculator;
--- src/pages/User/UserTimeClock.tsx
+++ src/pages/User/UserTimeClock.tsx
@@ -32,10 +32,9 @@ import {
 } from '@chakra-ui/react';
 import { checkLoginUser } from '../../components/auth/checkLoginUser';
 import Layout from '../../components/common/Layout';
-import { AddIcon, ChevronDownIcon, DeleteIcon } from '@chakra-ui/icons';
 import Swal from 'sweetalert2';
-import Pagination from '../../components/Pagination';
 import Geolocation from '@react-native-community/geolocation';
+import DistanceCalculator from '../../components/DistanceCalculator';
 
 interface TimeClock {
     timeclock_id: number;
@@ -46,69 +45,50 @@ interface TimeClock {
 
 const UserTimeClock = () => {
     const [timeclocks, setTimeClocks] = useState<TimeClock[]>([]);
-    const { isOpen: isOpenModal1, onOpen: onOpenModal1, onClose: onCloseModal1 } = useDisclosure();
-    const { isOpen: isOpenModal2, onOpen: onOpenModal2, onClose: onCloseModal2 } = useDisclosure();
-    const { isOpen: isOpenModal3, onOpen: onOpenModal3, onClose: onCloseModal3 } = useDisclosure();
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 5;
-    const totalPages = Math.ceil(timeclocks.length / itemsPerPage);
-    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [user_id, setUserId] = useState<number>(0);
     const [isInRange, setIsInRange] = useState(false);
-    const [formCreateData, setFormCreateData] = useState({
-        user_id: 0,
-        clock_in: '',
-    });
-    const [formUpdateData, setFormUpdateData] = useState({
-        timeclock_id: 0,
-        clock_out: '',
-
-    });
 
 
     useEffect(() => {
         fetchTimeClocks();
 
-        const watchId = Geolocation.watchPosition(
-            position => {
-                // ตำแหน่งปัจจุบันของผู้ใช้
-                const userLocation = {
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                };
-
-                // ตำแหน่งที่ต้องการให้ Check-in (ตั้งตำแหน่งที่ต้องการตรวจสอบ)
-                const targetLocation = {
-                    latitude: 18.77470470638687,
-                    longitude: 98.97101352036422,
-                    // 18.77470470638687, 98.97101352036422 home
-                    // 18.773604102368942, 98.9662712305405 office
-                };
-
-                // คำนวณระยะทางระหว่างตำแหน่งผู้ใช้กับตำแหน่งที่ต้องการให้ Check-in
-                const distance = calculateDistance(userLocation, targetLocation);
-
-                // ตรวจสอบว่าผู้ใช้อยู่ในระยะที่กำหนดหรือไม่
-                const isInRange = distance <= 50;
-
-                setIsInRange(isInRange);
-            },
-            error => console.error(error),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-        );
-
-        // ยกเลิกการตรวจสอบตำแหน่งเมื่อ Component ถูก Unmount
+        const watchId = startWatchingPosition(setIsInRange);
         return () => {
             Geolocation.clearWatch(watchId);
         };
     }, []);
 
+    console.log(isInRange)
+
+    const startWatchingPosition = (setIsInRangeCallback: (value: boolean) => void) => {
+        return Geolocation.watchPosition(
+            position => {
+                const userLocation = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                };
+
+                const targetLocation = {
+                    latitude: 18.773604102368942,
+                    longitude: 98.773604102368942,
+                    // 18.77470470638687, 98.97101352036422 home
+                    // 18.773604102368942, 98.773604102368942 office
+                };
+
+                const distance = DistanceCalculator(userLocation, targetLocation);
+                const isInRange = distance <= 10;
+
+                setIsInRangeCallback(isInRange);
+            },
+            error => console.error(error),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+    };
+
     const fetchTimeClocks = async () => {
         try {
             const decoded = await checkLoginUser();
-            setFormCreateData({
-                user_id: decoded.user_id,
-                clock_in: '',
-            });
+            setUserId(decoded.user_id);
             const token = localStorage.getItem('token');
             const response = await fetch(`${process.env.REACT_APP_API_URL}/timeclock/check_clock_in/${decoded.user_id}`, {
                 method: 'GET',
@@ -145,12 +125,76 @@ const UserTimeClock = () => {
         )
     }
 
-    const ClockIn = () => {
+    const ClockIn = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/timeclock/clock_in`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: 'Bearer ' + token,
+                },
+                body: JSON.stringify({ user_id: user_id }),
+            });
 
+            const result = await response.json();
+
+            if (result.status === 'success') {
+                await Swal.fire({
+                    icon: 'success',
+                    title: 'Clock In Successful',
+                    text: 'You have successfully clocked in.',
+                });
+                fetchTimeClocks();
+            } else if (result.status == 'error') {
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Failed to Clock In',
+                    text: 'There was an issue clocking in.',
+                });
+                fetchTimeClocks();
+            } else {
+                alert('Failed')
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
     }
 
-    const ClockOut = () => {
+    const ClockOut = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/timeclock/clock_out`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: 'Bearer ' + token,
+                },
+                body: JSON.stringify({ timeclock_id: timeclocks[0]?.timeclock_id }),
+            });
 
+            const result = await response.json();
+
+            if (result.status === 'success') {
+                await Swal.fire({
+                    icon: 'success',
+                    title: 'Clock Out Successful',
+                    text: 'You have successfully clocked out.',
+                });
+                fetchTimeClocks();
+            } else if (result.status == 'error') {
+                await Swal.fire({
+                    icon: 'error',
+                    title: 'Failed to Clock Out',
+                    text: 'There was an issue clocking out.',
+                });
+                fetchTimeClocks();
+            } else {
+                alert('Failed')
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
     }
 
     return (
@@ -170,32 +214,3 @@ const UserTimeClock = () => {
 }
 
 export default UserTimeClock
-
-const calculateDistance = (locationA: Location, locationB: Location) => {
-    const earthRadius = 6371; // รัศมีของโลก (หน่วย: กิโลเมตร)
-
-    const latA = toRadians(locationA.latitude);
-    const lonA = toRadians(locationA.longitude);
-    const latB = toRadians(locationB.latitude);
-    const lonB = toRadians(locationB.longitude);
-
-    const dLon = lonB - lonA;
-    const dLat = latB - latA;
-
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(latA) * Math.cos(latB) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    return earthRadius * c;
-};
-
-// ฟังก์ชั่นแปลงองศาเป็นเรเดียน
-const toRadians = (degrees: number) => (degrees * Math.PI) / 180;
-
-// ชนิดข้อมูลตำแหน่ง
-interface Location {
-    latitude: number;
-    longitude: number;
-};
