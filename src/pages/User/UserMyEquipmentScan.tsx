f6f96a551f12f88f0794ac0662c1ed41519c8ac4
NattawatTHwork <nattawatth.work@gmail.com>
2024-03-03 23:31:37 +0700

    scan

--- src/pages/User/UserMyEquipmentScan.tsx
+++ src/pages/User/UserMyEquipmentScan.tsx
@@ -62,12 +62,95 @@ const UserMyEquipmentScan = () => {
         setSelectedDeviceId(deviceId);
     };
 
+    const checkStatusEquipment = async (text: string) => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/borrow/equipment/${text}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + token,
+                },
+            });
+
+            const result = await response.json();
+            console.log(result)
+
+            if (result.status == 'success') {
+                if (result.message.length == 0 || result.message[0].status == 0) {
+                    fetchThisEquipment(parseInt(text)).then(equipment_data => {
+                        if (equipment_data[0]?.status == 1 && equipment_data[0]?.deleted == 1) {
+                            Swal.fire({
+                                title: `Do you want to borrow ${equipment_data[0].equipment}?`,
+                                icon: "question",
+                                showCancelButton: true,
+                                confirmButtonText: "Yes",
+                                confirmButtonColor: "#3182CE",
+                                cancelButtonText: "No",
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    console.log('confirm')
+                                    // fetch ข้อมูลลง database borrow
+                                }
+                            });
+                        } else {
+                            Swal.fire({
+                                title: "Error",
+                                text: "This equipment cannot be borrowed or has been deleted",
+                                icon: "error",
+                                confirmButtonColor: '#3182CE',
+                            });
+                        }
+                    })
+                } else {
+                    if (result.message[0].user_id == user_id) {
+                        // คุณต้องการคืนอุปกรณ์
+                        console.log('return')
+                    } else {
+                        // ให้คืนก่อน
+                        console.log('return before')
+                    }
+                }
+            } else {
+                console.log('fetch data task failed')
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const fetchThisEquipment = async (equipment_id: number) => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/equipment/${equipment_id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + token,
+                },
+            });
+
+            const result = await response.json();
+
+            if (result.status == 'success') {
+                return result.message;
+                // setFormUpdateData(result.message[0]);
+                // onOpenModal2();
+            } else {
+                console.log('fetch data equipment failed')
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
     return (
         <>
             <Layout>
                 <Box marginBottom="4">
                     <Scanner
-                        onResult={(text, result) => console.log(text, result)}
+                        onResult={(text, result) => checkStatusEquipment(text)}
                         onError={(error) => console.log(error?.message)}
                         options={{ deviceId: selectedDeviceId || undefined }}
                     />
